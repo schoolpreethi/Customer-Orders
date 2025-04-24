644dea2a02611f9aeda04a9a3159d6cc34c77a99
schoolpreethi <schoolpreethi789@gmail.com>
2025-04-24 10:32:14 +0530

    Add files via upload

---
+++ customer orders project.sql
@@ -0,0 +1,45 @@
+-- CREATE DATABASE Customer;
+
+-- SELECT * FROM customerorders;
+
+-- ALTER TABLE customerorders CHANGE `ï»¿Id` Id INT;
+
+-- 1. View all orders
+-- SELECT * FROM customerorders;
+
+-- 2.  List all distinct products sold
+-- SELECT DISTINCT Items FROM customerorders;
+
+-- 3.  Calculate average customer rating
+-- SELECT AVG(Rating) AS AVG_Rating FROM customerorders;
+
+-- 4. Total sales per customer
+-- SELECT Full_Name, SUM(Total_sales) AS total_spent
+-- FROM customerorders
+-- GROUP BY Full_Name
+-- ORDER BY total_spent DESC
+-- LIMIT 1000;
+
+-- 5. Top 5 best-selling products by revenue
+-- SELECT Items, SUM(Total_sales) AS  revenue
+-- FROM customerorders
+-- GROUP BY Items
+-- ORDER BY  revenue DESC
+-- LIMIT 5;
+
+-- 6. Customers with no orders in the last 3 months (assuming current date is 2021-12-31)
+-- SELECT Full_Name, EmailAdress
+-- FROM customerorders
+-- WHERE Full_Name NOT IN (
+--     SELECT DISTINCT Full_Name
+--     FROM customerorders
+--     WHERE Order_Datetime >= '2021-10-01'
+-- );
+
+
+
+
+
+
+
+
